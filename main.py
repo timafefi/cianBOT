84d322c894ee57772e85eec5cf6a7fe1591fd55b
Tim <timafefi@gmail.com>
2023-11-30 03:59:43 +0300

    added functionality for canceling subscriptions and handling Blocked exception

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import logging
 import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram import types, Bot, Dispatcher
-from cian import Cparser
 from aiogram.filters.command import Command
+from aiogram.exceptions import TelegramForbiddenError
+from cian import Cparser
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token='1115198779:AAHPsbIAg3UBSb4A-ZsulryV1LQdi3Ck2Hc')
+bot = Bot(token='6518377388:AAGS8yk8IOg0pV2RNPFpX1PgjnZoy4Aq1OA')
 scheduler = AsyncIOScheduler()
 dp = Dispatcher()
 cp = Cparser()
 ids = []
-
 
 def beautify_output(j):
     s = f'Цена: {j["price_per_month"]}\nКомнат: {j["rooms_count"]}. '\
         f'Площадь: {j["total_meters"]}м^2\nМетро: {j["underground"]}\n'\
         f'{j["link"]}'
     return s
+
+
 
 @dp.message(Command('start'))
 async def cmd_start(message: types.Message):
@@ -29,24 +31,29 @@ async def cmd_start(message: types.Message):
         ids.append(message.from_user.id)
     await message.answer(s)
 
+
 @dp.message(Command('cancel'))
 async def cmd_cancel(message: types.Message):
-    s = 'Вы больше не будете получать обновления\n'\
-        'Чтобы снова начать их получать, используйте /start'
-    ids.pop(message.from_user.id)
+    if message.from_user.id in ids:
+        ids.remove(message.from_user.id)
+    await message.answer('Вы отписались от рассылки\n. Чтобы возобновить её, наберите /start')
+
 
 async def get_new_flats():
     diff = cp.update()
-    print(diff)
-    print(ids)
     for i in ids:
         for out in reversed(diff):
-            await bot.send_message(i, beautify_output(out))
+            try:
+                await bot.send_message(i, beautify_output(out))
+            except TelegramForbiddenError:
+                logging.info(f'user {i} has blocked bot. Removing uid')
+                ids.remove(i)
+                break
 
 
 
 async def main():
-    scheduler.add_job(get_new_flats, "interval", seconds=300)
+    scheduler.add_job(get_new_flats, "interval", seconds=90)
     scheduler.start()
     await dp.start_polling(bot)
 
